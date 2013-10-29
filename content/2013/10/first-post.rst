9c8458c97f11a18f98dda57ad87e68bc648f5d0f
Hong Minhee <minhee@dahlia.kr>
2013-10-30 01:15:29 +0900

    Pelican configuration and the first post

---
+++ content/2013/10/first-post.rst
@@ -0,0 +1,23 @@
+The First Post
+==============
+
+:date: 2013-10-29T15:30:00Z
+:slug: first-post
+:author: Hong Minhee
+:email: minhee@dahlia.kr
+:summary: We started blogging
+
+Welcome to the Earth Reader blog.
+
+Earth Reader is a feed reader intended to be run on various devices and
+synchronized without any particular centralized proprietary services.
+There also is `Hong Minhee's blog post`__ about our initial motivation of
+the project.
+
+This is where we're going to drop all sorts of posts about what is going on
+in the Earth Reader project.  We will try to periodically update the blog.
+
+| Thanks,
+| Minhee and the Earth Reader team
+
+__ https://minhee.quora.com/RSS-readers-had-been-originally-desktop-apps
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import os
+
+
+AUTHOR = u'Earth Reader Team'
+SITENAME = u'Earth Reader Blog'
+SITEURL = 'http://blog.earthreader.org'
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = u'en'
+
+ARTICLE_EXCLUDES = ['pages', 'plugins']
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'index.xml'
+CATEGORY_FEED_ATOM = 'category/%s.xml'
+TAG_FEED_ATOM = 'tag/%s.xml'
+
+# Blogroll
+LINKS = [
+    ('Website', 'http://earthreader.org/'),
+    ('GitHub', 'https://github.com/earthreader')
+]
+
+# Social widget
+SOCIAL = []
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = os.environ.get('CI') != 'true'
+
+USE_FOLDER_AS_CATEGORY = False
+
+TYPOGRIFY = True
+
+PLUGIN_PATH = 'plugins'
+PLUGINS = [
+    'gravatar'
+]
