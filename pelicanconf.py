3a8979275a512c4f84c8c984c4292eb4079dbd2e
Jihyeok Seo <me@limeburst.net>
2013-11-10 06:17:14 +0900

    Change the permalink format

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,8 +16,8 @@ DEFAULT_LANG = u'en'
 
 ARTICLE_EXCLUDES = ['pages', 'plugins']
 
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
-ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/' \
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/' \
               if CI else ARTICLE_SAVE_AS
 
 
